fbee049d5483199a3b3fcb2d0bff4773a385960d
Derek Fitzer <franzderek@gmail.com>
2024-03-13 14:57:11 -0400

    rename test

--- ObservableObjSample/Data.swift
+++ ObservableObjSample/Data.swift
@@ -10,6 +10,7 @@ import Foundation
 class UserProgress: ObservableObject {
     @Published var score = 0
     @Published var playerName: String
+    // var title = ""
     init(score: Int = 0, playerName: String) {
         self.score = score
         self.playerName = playerName
